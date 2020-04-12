6ee4b4b199971322e5bd022957b765a418df2d55
Roman Zubko <skcorpoma@gmail.com>
2020-04-12 13:44:13 +0300

    Updates

---
+++ precache-manifest.5af63108fac3913d9d38e6c0188f40db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404d8a09ae9dd333489927e17f46ad87",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "16bc13ef92f86ac9406c",
+    "url": "/todo-app/static/css/main.e0e5e2c5.chunk.css"
+  },
+  {
+    "revision": "fea7eabb969eab7e787e",
+    "url": "/todo-app/static/js/2.22ffba3d.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/todo-app/static/js/2.22ffba3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16bc13ef92f86ac9406c",
+    "url": "/todo-app/static/js/main.43639cc9.chunk.js"
+  },
+  {
+    "revision": "c339d801fd14056574f5",
+    "url": "/todo-app/static/js/runtime-main.5f90965c.js"
+  }
+]);
